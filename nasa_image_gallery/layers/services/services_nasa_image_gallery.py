cb33a7be05873f652fb28e80b6e54d886d9f1088
Antonela Ledesma <ledesmaantonela20@gmail.com>
2024-06-21 19:16:37 -0300

    Add modified getAllImages function to file

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -6,19 +6,11 @@ from ..generic import mapper
 from django.contrib.auth import get_user
 
 def getAllImages(input=None):
-    # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
-    # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     json_collection = transport.getAllImages(input)
-
     images = []
-
-    # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
-    
     for object in json_collection:
-        if 'links' in object:
-            # Utilizamos el mapper para convertir el objeto JSON en NASACard
-            nasa_card = mapper.fromRequestIntoNASACard(object)
-            images.append(nasa_card)
+        nasa_card = mapper.fromRequestIntoNASACard(object)
+        images.append(nasa_card)
 
     return images
 
