ce847728832888149744e36d4e71d89a0b490364
Antonela Ledesma <ledesmaantonela20@gmail.com>
2024-06-26 23:39:01 -0300

    Remove duplicate admin and accounts path

--- nasa_image_gallery/urls.py
+++ nasa_image_gallery/urls.py
@@ -3,20 +3,9 @@ from django.urls import path,include #importamos include tambien
 from . import views
 
 urlpatterns = [
-    # Configuración de la administración de Django
-    path('admin/', admin.site.urls),
-
-    # URLs de autenticación de Django
-    path('accounts/', include('django.contrib.auth.urls')),
-
-    # Incluye las URLs de la aplicación 'pages'
-    #path('', include('pages.urls')),
-
-    # Resto de las URLs definidas en el proyecto principal
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
     path('home/', views.home, name='home'),
-
     path('buscar/', views.search, name='buscar'),
 
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
@@ -24,6 +13,5 @@ urlpatterns = [
     path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
 
     path('exit/', views.exit, name='exit'),
-    path('salir/',views.salir,name='salir'),
     
 ] 
